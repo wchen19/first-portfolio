7fa20d9857346471c542c3236c5ed18f7480ae73
Wilbert Chen <wilbertchen2001@gmail.com>
2022-05-10 20:58:38 +0700

    add footer section

---
+++ src/sections/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+const Footer = () => {
+  return <div className='footer'>Wilbert Chen's Portfolio Website</div>;
+};
+
+export default Footer;
